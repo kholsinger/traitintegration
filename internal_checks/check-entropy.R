78cb18c4a637e3aaad1ecc86cf4a80589c614266
kholsinger <keholsinger@gmail.com>
2025-07-30 08:48:23 -0400

    Minor update to entropy check

--- internal_checks/check-entropy.R
+++ internal_checks/check-entropy.R
@@ -8,7 +8,7 @@ dat <- tibble(r = NA,
               brute_force = NA,
               analytical  = NA)
 for (r in seq(0.1, 0.9, by = 0.1)) {
-  for (k in 3:15) {
+  for (k in 2:20) {
     brute_force <- entropy_omega(r, k)
     analytical <- k/2 + (k/2)*log(2*pi) + (1/2)*log(1 + (k - 1)*r) +
       (1/2)*(k - 1)*log(1 - r)
@@ -22,5 +22,9 @@ for (r in seq(0.1, 0.9, by = 0.1)) {
 dat <- dat %>%
   filter(!is.na(r)) %>%
   mutate(difference = abs(brute_force - analytical))
+sink("internal_checks/check-entropy-results.txt", split = TRUE)
+cat("r in seq(0.1, 0.9, by = 0.1)\n",
+    "k in 2:20\n", sep = "")
 cat("Maximum absolute difference: ", max(dat$difference))
+sink()
 
